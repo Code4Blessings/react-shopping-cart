b259021cba8c6663c157a852f9d04b57be078a47
Code4Blessings <robinwarden@code4blessings.com>
2019-12-09 18:58:37 -0500

    Added Context API to the Navigation and ShoppingCart components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
+
+//Contexts
 import { ProductContext } from "./contexts/ProductContext";
+import { CartContext } from "./contexts/CartContext";
 
 // Components
 import Navigation from './components/Navigation';
@@ -19,15 +22,13 @@ function App() {
 
 	return (
 		<div className="App">
-			<Navigation cart={cart} />
-				<ProductContext.Provider value={{products, addItem}} >
-					{/* Routes */}
-					<Route exact path="/" component={Products} />
-
-					<Route
-						path="/cart"
-						render={() => <ShoppingCart cart={cart} />}
-					/>
+			<ProductContext.Provider value={{products, addItem}}>	
+			<CartContext.Provider value={{cart}}>
+						<Navigation />
+						{/* Routes */}
+						<Route exact path="/" component={Products} />
+						<Route path="/cart" component={ShoppingCart} />
+			</CartContext.Provider>
 			</ProductContext.Provider>
 		</div>
 	);
